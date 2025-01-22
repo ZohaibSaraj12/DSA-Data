2ba5378ad3c5ba48818dbdcec9f3d8111e31b6d4
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2025-01-22 21:21:17 +0500

    init

--- Heap_Insertion.cpp
+++ Heap_Insertion.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 class Heap {
+    public:
     int size;
     int arr[100];
 
@@ -14,10 +15,42 @@ class Heap {
         size = size + 1;
         int index = size;
         arr[index] = value;
-        
+
         while (index > 1)
         {
-            
+            int parent = index / 2;
+            if (arr[parent] < arr[index])
+            {
+                swap(arr[parent], arr[index]);
+                index = parent;
+            }
+            else 
+            {
+                return;
+            }
         }
     }
+    void print()
+    {
+        cout << endl;
+        for (int i = 1; i <= size; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
 };
+int main()
+{
+    Heap h;
+    h.input(50);
+    h.input(55);
+    h.input(53);
+    h.input(52);
+    h.input(54);
+
+    h.print();
+
+    
+    return 0;
+}
