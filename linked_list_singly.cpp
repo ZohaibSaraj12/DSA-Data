845a61f988c8a28668d49f8fc58149bfdccf5a96
Zohaib Saraj <154296659+ZohaibSaraj12@users.noreply.github.com>
2025-02-23 13:42:48 +0500

    init

--- linked_list_singly.cpp
+++ linked_list_singly.cpp
@@ -1,172 +1,172 @@
 #include <iostream>
 using namespace std;
+class Node {
 
-class Node
-{
 public:
-    int data;
-    Node *next;
-
-    // Constructor
-    Node(int data)
-    {
-        this->data = data;
-        this->next = NULL;
-    }
-    // Destructor
-    ~Node()
-    {
-        if (this->next != NULL)
-        {
-            // next of this node will be NULL
-            this->next = NULL;
-            delete next;
-        }
-    }
+int data;
+Node* next;
+Node(int data)
+{
+    this->data = data;
+    this->next = NULL;
+}
+~Node()
+{
+    this->next = NULL;
+}
 };
-
-// Insert at Head
-void insertAtHead(Node *&head, Node *&tail, int data)
+void insertAtHead(Node*& head, Node*& tail, int data)
 {
-    Node *temp = new Node(data);
-    temp->next = head;
-    head = temp;
+    Node* newNode = new Node(data);
 
-    // If the list was empty, update the tail
-    if (tail == NULL)
+    if (head == NULL && tail == NULL)
     {
-        tail = temp;
+        head = tail = newNode;
         return;
     }
-}
 
-// Insert at Tail
-void insertAtTail(Node *&head, Node *&tail, int data)
+    newNode->next = head;
+    head = newNode;
+    
+
+}
+void insertAtTail(Node*& head, Node*& tail, int data)
 {
-    Node *temp = new Node(data);
-    if (tail == NULL)
-    { // Empty list
-        head = tail = temp;
+    Node* newNode = new Node(data);
+    if (head == NULL && tail == NULL)
+    {
+        tail = head = newNode;
         return;
     }
-    tail->next = temp;
-    tail = temp;
-}
 
-// Insert at Position
-void insertAtPosition(Node *&head, Node *&tail, int data, int position)
+    tail->next = newNode;
+    tail = newNode;
+}
+void insertAtPosition(Node*& head, Node*& tail, int data, int position)
 {
+    Node* temp = head;
     if (position <= 1)
-    { // Handle invalid or first position
+    {
         insertAtHead(head, tail, data);
         return;
     }
 
-    Node *temp = head;
     int count = 1;
-
-    // Traverse to the position before insertion point
     while (count < position - 1 && temp != NULL)
     {
         temp = temp->next;
         count++;
     }
 
-    // If position is beyond the list length, insert at tail
     if (temp == NULL)
     {
         insertAtTail(head, tail, data);
         return;
     }
 
-    // Insert in the middle
-    Node *nodeToInsert = new Node(data);
-    nodeToInsert->next = temp->next;
-    temp->next = nodeToInsert;
+    Node* newNode = new Node(data);
+    newNode->next = temp->next;
+    temp->next = newNode;
 
-    // If inserting at the last position, update tail
-    if (nodeToInsert->next == NULL)
+    if (temp == NULL)
     {
-        tail = nodeToInsert;
-    }
+        tail = newNode;
+        
+    } 
 }
-
-// Print the Linked List
-void printList(Node *head)
+void deleteNode(Node*& head, int data)
 {
-    Node *temp = head;
-    while (temp != NULL)
+    if (head == NULL)
     {
-        cout << temp->data << " ";
-        temp = temp->next;
+        cout << "No Node available in the list go forward and insert first!!!!" << endl;
+        return;
     }
-    cout << endl;
-}
-// deleting the node in list whether on 1st last and middle
-void deleteNode(int position, Node *&head)
-{
 
-    if (head == NULL) {
-        cout << "List is empty";
+    if (data == head->data)
+    {
+        Node* temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+    else 
+    {
+        Node* previous = NULL;
+        Node* current = head;
+
+        while (current != NULL)
+        {
+            if (current->data == data)
+            {
+                previous->next = current->next;
+                current->next = NULL;
+                delete current;
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+    }
+
+}
+void deleteNodeByPosition(Node*& head, Node*& tail, int position)
+{
+    Node* previous = NULL;
+    Node* current = head;
+    if (position < 1)
+    {
+        cout << "No node availabe" << endl;
         return;
     }
     if (position == 1)
     {
-        Node *previous = head;
-
+        Node* temp = head;
         head = head->next;
-        delete previous;
-        previous->next = NULL;
+        temp->next = NULL;
+        delete temp;
+        return;
     }
-
-    else
+    int count = 1;
+    while (count < position && current != NULL)
     {
-        Node *current = head;
-        Node *previous = NULL;
-
-        int count = 1;
-
-        while (count < position && current != NULL)
-        {
-            previous = current;
-            current = current -> next;
-            count++;
-        }
-        if (current != NULL) {
-            previous -> next = current -> next;
-            delete current;
-            current -> next = NULL;
-        }
+        previous = current;
+        current = current->next;
+        
+        count++;
+    }
+    
+    if(current != NULL) 
+    {
+    previous->next = current->next;
+    current->next = NULL;
+    delete current;
     }
 }
-// Main Function
+
+void print(Node*& head)
+{
+    Node *temp = head;
+    while(temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+}
 int main()
 {
-    Node *head = NULL;
-    Node *tail = NULL;
+    Node* head = NULL;
+    Node* tail = NULL;
 
-    // Test Cases
-    insertAtHead(head, tail, 10); // Insert at head
-    printList(head);
-
-    insertAtTail(head, tail, 20); // Insert at tail
-    printList(head);
-
-    insertAtHead(head, tail, 5); // Insert at head
-    printList(head);
-
-    insertAtPosition(head, tail, 15, 3); // Insert at position 3
-    printList(head);
-
-    insertAtPosition(head, tail, 25, 7); // Insert at tail (out of bounds)
-    printList(head);
-
-    insertAtPosition(head, tail, 1, 0); // Insert at head (invalid position)
-    printList(head);
+    insertAtHead(head, tail, 20);
+    insertAtHead(head, tail, 30);
+    insertAtTail(head, tail, 40);
+    insertAtTail(head, tail, 50);
+    insertAtTail(head, tail, 60);
+    insertAtTail(head, tail, 70);
+    insertAtPosition(head, tail, 90, 4);
+    deleteNodeByPosition(head, tail, 1);
+    print(head);
     
 
-
-    deleteNode(3, head);
-    printList(head);
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,1 @@
-n()
+elete temp;
